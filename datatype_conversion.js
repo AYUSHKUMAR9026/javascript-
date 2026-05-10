bcd9591ac5473392b2002fd63345bafc6c2d6059
AYUSHKUMAR9026 <rockayushkumar786@gmail.com>
2026-05-10 16:45:35 +0530

    Add files via upload

---
+++ datatype_conversion.js
@@ -0,0 +1,5 @@
+let abc="33";
+let num =Number(abc);
+console.log(num);
+let x=2>3;
+console.log(+x);
---
+++ non_primitive.js
@@ -0,0 +1,21 @@
+//array 
+const arr =["hello"];
+//object 
+let myobj={
+    name :"ayush ",
+    age :19
+}
+//function 
+const myfunction =function (){
+    console.log("hello world ");
+}
+console.log(myfunction ());
+/*Primitive Types:
+1. Stored in stack.
+2. Passed or assigned by value (call by value).
+3. Changes to copies don't affect the original.
+
+Reference Types:
+1. Stored in heap; accessed via references (stored in stack).
+2. Changing the value through another reference affects the original.*/
+
